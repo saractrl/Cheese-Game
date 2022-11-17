59fe30c7b12f5c91ab8f4864b6303e8ef69a3d19
saractrl <sara.ctrl@outlook.com>
2022-11-17 09:54:23 +0100

    09.54

--- Cheese-Game/Lines.cs
+++ Cheese-Game/Lines.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows;
+using System.Xml.Linq;
 
 namespace Cheese_Game
 {
@@ -15,46 +16,47 @@ namespace Cheese_Game
 
 
 
+
         // Changes the player color
         public void OnClick(object sender, RoutedEventArgs e)
         {
             Button b = (Button)sender;
-       
+
+
 
             if (this.turn)
             {
-              
+
                 b.Background = new SolidColorBrush(Color.FromRgb(0xC3, 0x00, 0xFF));
-                
+
             }
-            else 
+            else
             {
                 b.Background = new SolidColorBrush(Color.FromRgb(0xE7, 0xFF, 0x08));
             }
 
             b.IsEnabled = false;
             this.turn = !this.turn;
-       
+
+
             // Sets turn back to false, so the color changes again.
 
-            
+            //public void BoxDetect()
+            //{ 
+
+
+            //}
+
+
+
+
+
+
+            //public void OnNewGame(object sender, RoutedEventArgs e)
+            //{
+
+
+            //}
         }
-
-        public void BoxDetect()
-        { 
-
-
-        }
-
-
-
-
-
-
-        //public void OnNewGame(object sender, RoutedEventArgs e)
-        //{
-           
-
-        //}
     }
 }
--- Cheese-Game/MainWindow.xaml.cs
+++ Cheese-Game/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace Cheese_Game
 {
@@ -25,18 +29,65 @@ namespace Cheese_Game
         public Lines button = new();
         public Lines newgame = new();
 
+        List<List<Element>> elements = new List<List<Element>>();
+      
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 15; i++)
+            {
+                elements.Add(new Element());
+            }
+            foreach (Element element in elements)
+            {
+                int index = elements.FindIndex(e => e.Equals(element));
+
+                element.Right = new Site();
+
+                if (index == 0)
+                {
+                    element.Left = new Site();
+                }
+                else
+                {
+                    element.Left = elements[index - 1].Right;
+                }
+            }
+
+            for (int i = 0; i < 15; i++)
+            {
+                elements.Add(new Element());
+            }
+            foreach (Element element in elements)
+            {
+                int index = elements.FindIndex(e => e.Equals(element));
+
+                element.Top = new Site();
+
+                if (index == 0)
+                {
+                    element.Top = new Site();
+                }
+                else
+                {
+                    element.Left = elements[index - 1].Bottom;
+                }
+            }
+
+
+
+            DataContext = elements;
         }
-    
+
 
         // Is connected to the class "Lines", this changes the line colours.
         public void OnClick(object sender, RoutedEventArgs e)
         {
 
             button.OnClick(sender, e);
-        
+
         }
 
         // By clicking the button "HELP" a website will pop up, that explains how the game works.
@@ -50,5 +101,80 @@ namespace Cheese_Game
         //{
         //    newgame.OnNewGame(sender, e);
         //}
+        public class Element : INotifyPropertyChanged
+        {
+            public Site Left { get; set; }
+
+            public Site Right { get; set; }
+
+            public Site Bottom { get; set; }
+
+            public Site Top { get; set; }
+
+            public Brush Color
+            {
+                get
+                {
+                    return this.color;
+                }
+                set
+                {
+                    this.color = value;
+                    NotifyPropertyChanged();
+                }
+            }
+            private Brush color;
+
+            public Element()
+            {
+                Color = Brushes.Pink;
+            }
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+        public class Site : INotifyPropertyChanged
+        {
+            public Brush Color
+            {
+                get
+                {
+                    return this.color;
+                }
+                set
+                {
+                    this.color = value;
+                    //NotifyPropertyChanged();
+                }
+            }
+            private Brush color;
+
+            public Site()
+            {
+                Color = Brushes.Gray;
+            }
+
+            //public void Clicked(bool turn)
+            //{
+            //    Color = turn ? Brushes.Blue : Brushes.Red;
+            //}
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+
+            private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
     }
 }
